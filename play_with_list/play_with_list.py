d91416fa12f77d850e138756dff20159b029a1a3
onuremir <emironur22@gmail.comr>
2020-04-17 21:39:25 +0300

    remove duplicates added

--- play_with_list/play_with_list.py
+++ play_with_list/play_with_list.py
@@ -12,3 +12,14 @@ def duplicateZeros(self, arr):
     Explanation: After calling your function, the input array is modified to: [1,2,3]
     """
 
+def removeDuplicates(self, nums):
+    """
+    Given a sorted array nums, remove the duplicates in-place such that
+    each element appear only once and return the new length.
+    :type nums: List[int]
+    :rtype: int
+    Given nums = [0,0,1,1,1,2,2,3,3,4],
+    0our function should return length = 5, with the first five elements of nums
+    being modified to 0, 1, 2, 3, and 4 respectively.
+    It doesn't matter what values are set beyond the returned length.
+    """
